2a5cb11bd40b787b944e11b37e46348e0a0d5032
balu <baluk1219@gmail.com>
2018-02-06 02:25:18 +0530

    added sql syntax

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
-
+router.post('/',(req, res, next)=> {
+    console.log(req.body.testing);
+    res.json({
+        success: true,
+        data: req.body.testing
+    });
+});
 module.exports = router;
