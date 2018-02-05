4815bb16c41a7fafc3868593b222d862390e2ae6
balu <baluk1219@gmail.com>
2018-02-05 16:27:10 +0530

    setup done

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -21,8 +22,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
-app.use('/', index);
+app.use('/index', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
