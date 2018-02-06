12bf0df04107218437ef236146a2964c8cd0d2ea
balu <baluk1219@gmail.com>
2018-02-07 00:46:49 +0530

    add callrecord data fetch

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  mysql = require('mysql'),
+  busboyBodyParser = require('busboy-body-parser'),
+  cors = require('cors');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-var mysql = require('mysql');
+const index = require('./routes/index'),
+  users = require('./routes/users');
 
 // var con = mysql.createConnection({
 //   host: "localhost",
@@ -29,15 +30,17 @@ var mysql = require('mysql');
 // });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// // app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(busboyBodyParser({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
