1ee1af59e32b6f6b364cbe2929ae717af3a662ef
balu <baluk1219@gmail.com>
2018-02-16 00:14:33 +0530

    changes

---
+++ public/app/services/profileService.js
@@ -0,0 +1,48 @@
+// angular.module('profileService', [])
+//     .factory('ProfileData')
+
+//     function ProfileData ($http, $q){
+//         const dataSentFromApi = {};
+
+//         dataSentFromApi.sendDataToApi = (file, data, $window)=> {
+//             let tokenAuth = $window.localStorage.getItem('token');
+//             console.log(tokenAuth);
+//             console.log("data is received : "+data);
+//             let url = "/index/fetchData"
+//             return http.post(url, {
+//                 file, data
+//             }).then((data)=> {
+//                 console.log(data);
+//                 return data.data;
+//             }).catch((err)=> {
+//                 console.log(err);
+//             });
+//         };
+
+//         return dataSentFromApi;
+//     };
+
+angular.module('profileService', [])
+    .factory('Profiledata', Profiledata);
+
+    function Profiledata($http, $window) {
+        const profile = {};
+        console.log('in profiledata');
+        profile.sendDataToApi = (file)=> {
+            // let requestData = [
+            //     file, data
+            // ];
+            let tokenAuth = $window.localStorage.getItem('token');
+            console.log(file);
+            let url = `/index/fetchData?token=${tokenAuth}`
+            console.log(url);
+            // let val = {a:'a', file};
+            return $http.post(url, file, { transformRequest: angular.identity, headers: {'Content-Type': undefined} }).then((data)=>{
+                console.log(data);
+                return data;
+            }).catch((err)=> {
+                console.log(err);
+            });
+        };
+        return profile;
+    };
---
+++ routes/admin/findData.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+const findData = (req, res, next)=> {
+    let connectFindData = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "mycalldb"
+    });
+    
+    connectFindData.connect((err)=> {
+        if(err) return err;
+        let sql = "";
+        connectFindData.query(sql, (err, results)=> {
+            console.log(results);
+            res.status(200).send({
+                success: true,
+                message: 'vallues',
+                data: results
+            });
+        });
+    });
+};
+
+module.exports = {
+    findData
+};
