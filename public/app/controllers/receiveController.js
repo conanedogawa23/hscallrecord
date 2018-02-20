e0a23679ad5b56ae07de0095d324304fa0ae75a9
balu <baluk1219@gmail.com>
2018-02-20 06:35:01 +0530

    send data

--- public/app/controllers/receiveController.js
+++ public/app/controllers/receiveController.js
@@ -4,13 +4,14 @@ angular.module('receiveCtrl', ['receiveService'])
     function ReceiveController($scope, receivedata){
         const vm = this;
         console.log('in receive controller');
-        let location = 'hyderabad';
-
+        vm.location = 'hyderabad';
+        vm.status = false;
         vm.requestDataApi = ()=>{
             console.log("in requestDataAPi");
-            receivedata.getData(location).then((data)=> {
+            receivedata.getData(vm.location).then((data)=> {
                 vm.srcData = data[0].audiopath;
                 console.log(vm.srcData);
+                vm.status = true;
             }).catch((err)=> {
                 console.log(err);
             });
