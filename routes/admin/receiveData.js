127b1db92072754fbd9d40a01f41ff392b993c51
balu <baluk1219@gmail.com>
2018-02-27 04:40:36 +0530

    added audio url

--- routes/admin/receiveData.js
+++ routes/admin/receiveData.js
@@ -4,8 +4,8 @@ const mysql = require('mysql'),
 const retrieveData = (req, res, next)=> {
     let connectFetchData = mysql.createConnection({
         host: "localhost",
-        user: "root",
-        password: "",
+        user: "phpmyadmin",
+        password: "mysql",
         database: "mycalldb"
     });
     
